58d7dcc7315a7a2a41de312e89392d75771b5b91
Eric Ene <123761606+eric-ene@users.noreply.github.com>
2023-01-27 18:53:48 -0500

    Add files via upload

---
+++ subbar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 16
+
+int main(void) {
+    char buffer[SIZE];
+    FILE* xprop = popen("xprop -name 'Polybar tray window' _NET_WM_PID | grep -Eo \"[0-9]+\"", "r");
+
+    fgets(buffer, SIZE, xprop);
+    buffer[SIZE - 1] = '\0';
+    
+    char kill[SIZE] = "kill ";
+
+    strcat(kill, buffer);
+
+    if (buffer[0] == NULL) {
+        system("polybar tray & disown");
+    } else {
+        system(kill);
+    }
+
+    fclose(xprop);
+}
